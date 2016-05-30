3e6e1da6b779ae00afd9b0cfb53529dcc25b447c
isabelrios <isabelrios@gmail.com>
2016-05-30 18:34:26 +0200

    sdk for react-native

--- android/app/src/main/java/org/mozilla/magnet/MainActivity.java
+++ android/app/src/main/java/org/mozilla/magnet/MainActivity.java
@@ -7,6 +7,8 @@ import com.facebook.react.shell.MainReactPackage;
 import java.util.Arrays;
 import java.util.List;
 
+import com.testfairy.react.TestFairyPackage;
+
 public class MainActivity extends ReactActivity {
 
     /**
@@ -35,7 +37,8 @@ public class MainActivity extends ReactActivity {
     protected List<ReactPackage> getPackages() {
         return Arrays.<ReactPackage>asList(
             new MainReactPackage(),
-                new MyAppPackage()
+                new MyAppPackage(),
+                new TestFairyPackage() // Add package
         );
     }
 }
--- index.android.js
+++ index.android.js
@@ -7,6 +7,8 @@ var ReactNative = require('react-native');
 var App = require('./lib/views/app');
 var React = require('react');
 
+const TestFairy = require('react-native-testfairy');
+
 var {
   UIManager,
   AppRegistry
@@ -21,4 +23,5 @@ class AndroidApp extends React.Component {
 // layout-animations are turned off on android by default
 UIManager.setLayoutAnimationEnabledExperimental(true);
 
+TestFairy.begin('79f84031feb1f2859326d1cedb5689a9b16933a8');
 AppRegistry.registerComponent('Magnet', () => AndroidApp);
